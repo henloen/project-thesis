3efe7dc76ef3853d8faf984dbddfca50718b9374
thombor <thombor@IOT-S-K-203.win.ntnu.no>
2015-12-05 11:28:53 +0100

    Java for running VBM + Restructured data

---
+++ mosel/src/VBM Solver/compileVBMRunner.cmd
@@ -0,0 +1,5 @@
+X:
+cd Desktop\GitHub\project-thesis\mosel\src\VBM Solver
+set path=%path%;X:\Documents\Java\jdk1.8.0_65\bin
+javac -cp .;lib/* VBMRunner.java
+cmd /k
---
+++ mosel/src/VBM Solver/runVBMRunner.cmd
@@ -0,0 +1,5 @@
+x:
+cd Desktop\GitHub\project-thesis\mosel\src\VBM Solver
+set path=%path%;X:\Documents\Java\jre1.8.0_65\bin
+java -cp .;lib/* VBMRunner
+cmd /k
