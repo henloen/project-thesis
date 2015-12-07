14dad37544d4671107a188f6bf0b30c906061250
henloen <henrilo@stud.ntnu.no>
2015-12-07 09:34:29 +0100

    more test data

--- java/src/voyageGenerationDP/Main.java
+++ java/src/voyageGenerationDP/Main.java
@@ -23,8 +23,8 @@ public class Main {
 	private static String inputFileName = "data/input/Input data.xls",
 			outputFileName = "data/output/"; //sets the folder, see the constructor of IO for the filename format
 	//heuristic parameters
-	private static int removeLongestArcs = 5, minInstallationsHeur = 0, maxArcsRemovedInstallation = 2;
-	private static double capacityFraction = 0.8;
+	private static int removeLongestArcs = 0, minInstallationsHeur = 0, maxArcsRemovedInstallation = 2;
+	private static double capacityFraction = 0.6;
 	
 	public static void main(String[] args) {
 		startTime = System.nanoTime();
